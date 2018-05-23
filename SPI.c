8cdffb5139399b3684faf873efd708174c52a29f
amepila <ie704453@iteso.mx>
2018-05-23 16:04:23 -0500

    Transfers SPI

--- SPI.c
+++ SPI.c
@@ -7,10 +7,13 @@
 #define SPI_MASTER          (0x40)
 #define SPI_CLEAR_SSPSTAT   (0x3F)
 #define SPI_CLEAR_SSPCON    (0xC0)
+#define SPI_SCLOCK_4        (0x00)
+#define SPI_SCLOCK_16       (0x01)
+#define SPI_SCLOCK_64       (0x02)
+#define SPI_ENABLE_PINS     (0x20)
 
 
-void SPI_enableClock
-(SPI_PhaseType phase, SPI_PolarityType cpol)
+static void SPI_enableClock(SPI_PhaseType phase, SPI_PolarityType cpol)
 {
     /**Clear the SPI registers*/
     SSPSTAT &= SPI_CLEAR_SSPSTAT;
@@ -44,19 +47,49 @@ void SPI_enableClock
     }
 }
 
-void SPI_enablePINS
-(SPI_SerialClk serial)
+static void SPI_enablePINS(SPI_SerialClk serial)
 {
     switch(serial)
     {
         case SPI_SERIAL_CLK4:
+            SSPCON &= SPI_SCLOCK_4;
             break;
         case SPI_SERIAL_CLK16:
+            SSPCON |= SPI_SCLOCK_16;
             break;
         case SPI_SERIAL_CLK64:
+            SSPCON |= SPI_SCLOCK_64;
             break;
         default:
             break;
     }
     
+    /**Clock pin as output*/
+    TRISCbits.TRISC5 = 0;
+    /**SDI pin as input*/
+    TRISCbits.TRISC4 = 1;
+    /**SDO pin as output*/
+    TRISCbits.TRISC7 = 0;
+    /**CS pin as output*/
+    TRISCbits.TRISC6 = 0;
+    
+    /**Enable pins of SPI*/
+    SSPCON |= SPI_ENABLE_PINS;
+    /**Disable the ChipSelect*/
+    PORTCbits.RC6 = 1;
 }
+
+uint8_t SPI_write(uint8_t data)
+{
+    /**Enable the Chip Select*/
+    PORTCbits.RC6 = 0;
+    
+    
+    
+    /*Disable the Chip Select*/
+    PORTCbits.RC6 = 1;
+}
+
+
+
+
